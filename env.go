02f897f7d3131309022a62793f788a3f29b5b31a
dilyara4949 <mukhambetovad49@mail.ru>
2024-05-30 17:03:09 +0500

    fix: add env structure

---
+++ env.go
@@ -0,0 +1,13 @@
+package employees_api
+
+import "os"
+
+type Env struct {
+	JWTTokenSecret string
+}
+
+func NewEnv() *Env {
+	env := Env{}
+	env.JWTTokenSecret = os.Getenv("JWTTokenSecret")
+	return &env
+}
