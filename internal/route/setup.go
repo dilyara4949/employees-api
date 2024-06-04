aab30a0eff72e8ec34d4f412f6b60c1b8a227230
dilyara4949 <mukhambetovad49@mail.ru>
2024-06-04 13:49:50 +0500

    fix: rename logCorrelationIDTimer

--- internal/route/setup.go
+++ internal/route/setup.go
@@ -12,20 +12,20 @@ func SetUpRouter(employeesController *controller.EmployeesController, positionsC
 
 	//auth := middleware.NewJWTAuth(config.JWTTokenSecret)
 
-	mux.HandleFunc("GET /positions/{id}", LogCorrelationIDTimer(positionsController.GetPosition, config.JWTTokenSecret))
-	mux.HandleFunc("POST /positions", LogCorrelationIDTimer(positionsController.CreatePosition, config.JWTTokenSecret))
-	mux.HandleFunc("DELETE /positions/{id}", LogCorrelationIDTimer(positionsController.DeletePosition, config.JWTTokenSecret))
-	mux.HandleFunc("PUT /positions/{id}", LogCorrelationIDTimer(positionsController.UpdatePosition, config.JWTTokenSecret))
-	mux.HandleFunc("GET /positions", LogCorrelationIDTimer(positionsController.GetAllPositions, config.JWTTokenSecret))
+	mux.HandleFunc("GET /positions/{id}", logCorrelationIDTimer(positionsController.GetPosition, config.JWTTokenSecret))
+	mux.HandleFunc("POST /positions", logCorrelationIDTimer(positionsController.CreatePosition, config.JWTTokenSecret))
+	mux.HandleFunc("DELETE /positions/{id}", logCorrelationIDTimer(positionsController.DeletePosition, config.JWTTokenSecret))
+	mux.HandleFunc("PUT /positions/{id}", logCorrelationIDTimer(positionsController.UpdatePosition, config.JWTTokenSecret))
+	mux.HandleFunc("GET /positions", logCorrelationIDTimer(positionsController.GetAllPositions, config.JWTTokenSecret))
 
-	mux.HandleFunc("GET /employees/{id}", LogCorrelationIDTimer(employeesController.GetEmployee, config.JWTTokenSecret))
-	mux.HandleFunc("POST /employees", LogCorrelationIDTimer(employeesController.CreateEmployee, config.JWTTokenSecret))
-	mux.HandleFunc("DELETE /employees/{id}", LogCorrelationIDTimer(employeesController.DeleteEmployee, config.JWTTokenSecret))
-	mux.HandleFunc("PUT /employees/{id}", LogCorrelationIDTimer(employeesController.UpdateEmployee, config.JWTTokenSecret))
-	mux.HandleFunc("GET /employees", LogCorrelationIDTimer(employeesController.GetAllEmployees, config.JWTTokenSecret))
+	mux.HandleFunc("GET /employees/{id}", logCorrelationIDTimer(employeesController.GetEmployee, config.JWTTokenSecret))
+	mux.HandleFunc("POST /employees", logCorrelationIDTimer(employeesController.CreateEmployee, config.JWTTokenSecret))
+	mux.HandleFunc("DELETE /employees/{id}", logCorrelationIDTimer(employeesController.DeleteEmployee, config.JWTTokenSecret))
+	mux.HandleFunc("PUT /employees/{id}", logCorrelationIDTimer(employeesController.UpdateEmployee, config.JWTTokenSecret))
+	mux.HandleFunc("GET /employees", logCorrelationIDTimer(employeesController.GetAllEmployees, config.JWTTokenSecret))
 }
 
-func LogCorrelationIDTimer(endpoint http.HandlerFunc, JWTTokenSecret string) http.HandlerFunc {
+func logCorrelationIDTimer(endpoint http.HandlerFunc, JWTTokenSecret string) http.HandlerFunc {
 	auth := middleware.NewJWTAuth(JWTTokenSecret)
 	middlewares := []middleware.Middleware{
 		auth.Auth(),
