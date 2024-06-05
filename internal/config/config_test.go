d09d114225d9440534b114c91048d229e24474ee
dilyara4949 <mukhambetovad49@mail.ru>
2024-06-05 10:33:55 +0500

    fix: config test

--- internal/config/config_test.go
+++ internal/config/config_test.go
@@ -6,10 +6,13 @@ func TestNewConfig(t *testing.T) {
 	cfg, err := NewConfig()
 
 	if cfg.JWTTokenSecret == "" {
-		t.Fatal("jwt token is empty")
+		t.Fatal("config jwt token secret is empty")
 	}
 	if cfg.Port == "" {
-		t.Fatal("jwt token is empty")
+		t.Fatal("config port token is empty")
+	}
+	if cfg.Address == "" {
+		t.Fatal("config address is empty")
 	}
 	if err != nil {
 		t.Fatal(err)
