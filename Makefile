8bb777cea6ba08ff90182850789de5ec0fc7b1f8
dilyara4949 <mukhambetovad49@mail.ru>
2024-06-10 16:54:02 +0500

    fix: reemove old entries

--- Makefile
+++ Makefile
@@ -1,14 +1,8 @@
 proto:
 	protoc protobuf/employee.proto --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative
 
-rest:
-	JWT_TOKEN_SECRET=my_secret_key REST_PORT=8080 GRPC_PORT=50052 ADDRESS=0.0.0.0 go run cmd/main.go
-
-grpc:
-	JWT_TOKEN_SECRET=my_secret_key REST_PORT=8080 GRPC_PORT=50052 ADDRESS=0.0.0.0 go run cmd/grpc/main.go
-
 test:
 	go test ./...
 
-both:
-	JWT_TOKEN_SECRET=my_secret_key REST_PORT=8080 GRPC_PORT=50052 ADDRESS=0.0.0.0 go run cmd/rest_grpc/main.go
+run:
+	JWT_TOKEN_SECRET=my_secret_key REST_PORT=8080 GRPC_PORT=50052 ADDRESS=0.0.0.0 go run cmd/main.go
--- cmd/main.go
+++ cmd/main.go
@@ -3,32 +3,63 @@ package main
 import (
 	"fmt"
 	conf "github.com/dilyara4949/employees-api/internal/config"
-	"log"
-	"net/http"
-
 	"github.com/dilyara4949/employees-api/internal/controller"
+	"github.com/dilyara4949/employees-api/internal/grpc/server"
 	"github.com/dilyara4949/employees-api/internal/repository/employee"
 	"github.com/dilyara4949/employees-api/internal/repository/position"
-	"github.com/dilyara4949/employees-api/internal/repository/storage"
 	"github.com/dilyara4949/employees-api/internal/route"
+	pb "github.com/dilyara4949/employees-api/proto"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"log"
+	"net"
+	"net/http"
 )
 
 func main() {
-	positionsStorage := storage.NewPositionsStorage()
-	employeesStorage := storage.NewEmployeesStorage()
+	positionsStorage := position.NewPositionsStorage()
+	employeeStorage := employee.NewEmployeesStorage()
 
 	positionRepo := position.NewPositionsRepository(positionsStorage)
-	positionController := controller.NewPositionsController(positionRepo)
-
-	employeeRepo := employee.NewEmployeesRepository(employeesStorage, positionRepo)
-	employeeController := controller.NewEmployeesController(employeeRepo)
-
-	mux := http.NewServeMux()
+	employeeRepo := employee.NewEmployeesRepository(employeeStorage, positionRepo)
 
 	config, err := conf.NewConfig()
 	if err != nil {
 		log.Fatalf("Error while getting config: %s", err)
 	}
+
+	go func() {
+		positionServer := server.NewPositionServer(positionRepo)
+		employeeServer := server.NewEmployeeServer(employeeRepo)
+
+		listen, err := net.Listen("tcp", fmt.Sprintf("%s:%s", config.Address, config.GrpcPort))
+		if err != nil {
+			log.Fatalf("Could not listen on port: %v", err)
+		}
+
+		s := grpc.NewServer(
+			grpc.ChainUnaryInterceptor(
+				server.CorrelationIDInterceptor(),
+				server.LoggingInterceptor,
+			),
+			grpc.StreamInterceptor(server.StreamLoggingInterceptor),
+		)
+		pb.RegisterPositionServiceServer(s, positionServer)
+		pb.RegisterEmployeeServiceServer(s, employeeServer)
+
+		reflection.Register(s)
+
+		if err := s.Serve(listen); err != nil {
+			log.Fatalf("Failed to serve: %v", err)
+		}
+
+		log.Printf("Hosting server on: %s", listen.Addr().String())
+	}()
+
+	positionController := controller.NewPositionsController(positionRepo)
+	employeeController := controller.NewEmployeesController(employeeRepo)
+
+	mux := http.NewServeMux()
 
 	route.SetUpRouter(employeeController, positionController, config, mux)
 
